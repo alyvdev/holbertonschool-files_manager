a2805d792d5a19fea33da378147d3fa9ffc3f0e4
alyvdev <alyvofficial@gmail.com>
2025-05-29 12:06:03 +0400

    Implement MongoDB and Redis client classes for database interactions

---
+++ utils/redis.mjs
@@ -0,0 +1,36 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => {
+      console.log('Redis client not connected to the server: ', err.message);
+    });
+    this.client.on('connect', () => {
+      console.log('Redis client connected to the server');
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.set).bind(this.client);
+    return setAsync(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    return delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
