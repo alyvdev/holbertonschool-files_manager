bb9ae7879d8e3f3f896b3d8c76a7264d8ca872eb
alyvdev <alyvofficial@gmail.com>
2025-05-29 12:08:26 +0400

    Refactor DBClient to improve connection validation and ensure proper database checks

--- utils/db.mjs
+++ utils/db.mjs
@@ -5,6 +5,7 @@ class DBClient {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
+
     const url = `mongodb://${host}:${port}`;
     this.client = new MongoClient(url, { useUnifiedTopology: true });
     this.client.connect().then(() => {
@@ -15,7 +16,7 @@ class DBClient {
   }
 
   isAlive() {
-    return !!this.client && !!this.db;
+    return !!this.client && !!this.client.topology && this.client.topology.isConnected();
   }
 
   async nbUsers() {
